f4968e65b1885203c838e66503d15b961dfa8c52
Mattias Petter Johansson <mpj@spotify.com>
2012-10-25 01:39:27 +0200

    Updated overpowered publish script

--- publish_patch.sh
+++ publish_patch.sh
@@ -1,11 +1,13 @@
 # Dangerously fast way to publish a patch to NPM
+set -e # Stop if anything fails
 npm install
 ./build.sh
 VERSION=`./node_modules/grunt/bin/grunt bump:patch |  grep -no 'to .*$' | cut -c 6-`;
 git add package.json
 git add lib/*.js
-git commit -m "Bumped version number to "$VERSION
+git commit -m "Bumped version number to "$VERSION"."
 git tag -a v$VERSION -m 'Version '$VERSION
-git push origin master --tags
 npm publish
+git push origin master --tags
+
 
