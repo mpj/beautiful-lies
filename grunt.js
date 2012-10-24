ba34a55db88a911bbaeec39277860581f16de327
Mattias Petter Johansson <mpj@spotify.com>
2012-10-25 00:54:24 +0200

    Added grunt build script

---
+++ grunt.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    meta: {
+      banner:
+        '// Beautiful Lies\n' +
+        '// version: <%= pkg.version %>\n' +
+        '// author: <%= pkg.author %>\n' +
+        '// license: <%= pkg.license %>'
+    },
+    simplemocha: {
+      all: {
+        src: 'test/**/*.coffee',
+        options: {
+          globals: ['should'],
+          timeout: 3000,
+          ui: 'bdd',
+          reporter: 'spec'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-simple-mocha');
+
+  grunt.registerTask('compile', 'Compiles CoffeeScript source into JavaScript.', function(){
+    var coffee = require('coffee-script');
+    var js = coffee.compile(grunt.file.read('create_liar.coffee'));
+    var banner = grunt.task.directive('<banner:meta.banner>', function() { return null; });
+    if (js) grunt.file.write('lib/create_liar.js', banner + js);
+  });
+
+  grunt.registerTask('build', 'simplemocha compile');
+};
